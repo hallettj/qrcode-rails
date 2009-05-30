08e6d19cc3041e95f8defdd03559cb1d89145529
Jesse Hallett <hallettj@gmail.com>
2009-05-29 23:53:51 -0700

    Force exclusion of ETag header in image responses.

--- app/controllers/qrcode_controller.rb
+++ app/controllers/qrcode_controller.rb
@@ -50,8 +50,9 @@ class QrcodeController < ApplicationController
 
   def image
     @qrimage = QRImage.find_by_md5(params[:md5])
-	if @qrimage
+    if @qrimage
       headers['Cache-Control'] = 'public; max-age=2592000' # cache image for a month
+      headers['Etag'] = nil
       send_data @qrimage.data, :filename => @qrimage.filename, :disposition => 'inline', :type => "image/png"
     else
       render :nothing => true, :status => 404
